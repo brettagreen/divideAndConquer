90b76f5e98715014032c4132e6347c645e1baf4f
Brett Green <brettalangreen@gmail.com>
2023-07-23 11:12:09 -0500

    clean up find-floor

--- find-floor.js
+++ find-floor.js
@@ -5,22 +5,16 @@ function findFloor(arr, val) {
     let middleIdx;
     let middleVal;
 
-    debugger
-
     while (leftIdx <= rightIdx) {
 
         middleIdx = Math.floor((leftIdx + rightIdx) / 2);
         middleVal = arr[middleIdx];
 
-        if (middleVal <= val && (arr[middleIdx - 1] < val && arr[middleIdx + 1] > val)) {
+        if (middleVal <= val && arr[middleIdx + 1] > val) {
             return middleVal;
-        } else if (middleVal <= val && (arr[middleIdx - 1] === val && arr[middleIdx + 1] > val)) {
-            rightIdx = middleIdx - 1;
-        } else if (middleVal <= val && arr[middleIdx + 1] === val) {
-            leftIdx = middleIdx + 1;      
         } else if (middleVal > val) {
             rightIdx = middleIdx - 1;
-        } else if (middleVal <= val && (arr[middleIdx -1] < val && arr[middleIdx + 1] < val)) {
+        } else if (middleVal <= val && (arr[middleIdx -1] <= val && arr[middleIdx + 1] <= val)) {
             leftIdx = middleIdx + 1;
         } else if (!arr[middleIdx + 1]) {
             break;
@@ -33,5 +27,6 @@ console.log(findFloor([1,2,8,10,10,12,19], 9)) // 8
 console.log(findFloor([1,2,8,10,10,12,19], 20)) // 19
 console.log(findFloor([1,2,8,10,10,12,19], 0)) // -1
 console.log(findFloor([12,15,16,19,19,19,19,20,25,30], 20)) // 20
+console.log(findFloor([1,2,3,3,3,3,3,3,3,3,3,3,3,5,9], 3))
 
 module.exports = findFloor
